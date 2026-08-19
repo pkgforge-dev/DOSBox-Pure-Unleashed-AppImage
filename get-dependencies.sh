619a788a095dab9b221a9d550c4eb0725c3a86e1
Link4Electronics <link4electronics@gmail.com>
2026-08-19 02:25:51 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -12,7 +12,7 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-echo "Making nightly build of DOSBox Pure Unleashed..."
+echo "Building DOSBox Pure Unleashed..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/schellingb/dosbox-pure-unleashed"
 VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
